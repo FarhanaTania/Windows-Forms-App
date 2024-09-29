90b7d97342133bdc396149dfc3b1390cca78b36b
Farhana Tania <ftania1@my.centennialcollege.ca>
2024-09-29 11:15:01 -0400

    first commit

---
+++ Lab10/bin/Debug/frmPrimeMinisters.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PrimeMinisters
+{
+    partial class frmPrimeMinisters
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrimeMinisters));
+            this.picPhoto = new System.Windows.Forms.PictureBox();
+            this.lstPrimeMinisters = new System.Windows.Forms.ListBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblTerm = new System.Windows.Forms.Label();
+            this.lblParty = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPhoto
+            // 
+            this.picPhoto.Image = global::PrimeMinisters.Properties.Resources.turner;
+            this.picPhoto.Location = new System.Drawing.Point(74, 66);
+            this.picPhoto.Name = "picPhoto";
+            this.picPhoto.Size = new System.Drawing.Size(250, 250);
+            this.picPhoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picPhoto.TabIndex = 0;
+            this.picPhoto.TabStop = false;
+            // 
+            // lstPrimeMinisters
+            // 
+            this.lstPrimeMinisters.FormattingEnabled = true;
+            this.lstPrimeMinisters.ItemHeight = 16;
+            this.lstPrimeMinisters.Location = new System.Drawing.Point(440, 76);
+            this.lstPrimeMinisters.Name = "lstPrimeMinisters";
+            this.lstPrimeMinisters.Size = new System.Drawing.Size(187, 84);
+            this.lstPrimeMinisters.TabIndex = 1;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(129, 390);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(70, 25);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "label1";
+            // 
+            // lblTerm
+            // 
+            this.lblTerm.AutoSize = true;
+            this.lblTerm.Location = new System.Drawing.Point(71, 445);
+            this.lblTerm.Name = "lblTerm";
+            this.lblTerm.Size = new System.Drawing.Size(44, 16);
+            this.lblTerm.TabIndex = 3;
+            this.lblTerm.Text = "label1";
+            // 
+            // lblParty
+            // 
+            this.lblParty.AutoSize = true;
+            this.lblParty.Location = new System.Drawing.Point(71, 485);
+            this.lblParty.Name = "lblParty";
+            this.lblParty.Size = new System.Drawing.Size(44, 16);
+            this.lblParty.TabIndex = 4;
+            this.lblParty.Text = "label1";
+            // 
+            // frmPrimeMinisters
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.lblParty);
+            this.Controls.Add(this.lblTerm);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lstPrimeMinisters);
+            this.Controls.Add(this.picPhoto);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrimeMinisters";
+            this.Text = "Prime Ministers";
+            this.Load += new System.EventHandler(this.frmPrimeMinisters_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.picPhoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPhoto;
+        private System.Windows.Forms.ListBox lstPrimeMinisters;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblTerm;
+        private System.Windows.Forms.Label lblParty;
+    }
+}
+
---
+++ bin/Debug/frmPrimeMinisters.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Web.Script.Serialization;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeMinisters
+{
+    public partial class frmPrimeMinisters : Form
+    {
+        private Dictionary<string, PrimeMinister> primeMinisters = new Dictionary<string, PrimeMinister>();
+
+        public frmPrimeMinisters()
+        {
+            InitializeComponent();
+            Load += frmPrimeMinisters_Load;  
+            lstPrimeMinisters.SelectedValueChanged += lstPrimeMinisters_SelectedValueChanged;
+        }
+
+        private void frmPrimeMinisters_Load(object sender, EventArgs e)
+        {
+            using (StreamReader file = File.OpenText("PrimeMinisters.json"))
+            {
+                JavaScriptSerializer serializer = new JavaScriptSerializer();
+                primeMinisters = serializer.Deserialize<Dictionary<string, PrimeMinister>>(file.ReadToEnd());
+            }
+
+            lstPrimeMinisters.DataSource = primeMinisters.Keys.ToList<string>();
+        }
+
+
+        private void lstPrimeMinisters_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (lstPrimeMinisters.SelectedItem != null)
+            {
+                string selectedLastName = lstPrimeMinisters.SelectedItem.ToString();
+
+                PrimeMinister selectedPM = primeMinisters[selectedLastName];
+
+                lblName.Text = $"{selectedPM.FirstName} {selectedPM.LastName}";
+                lblTerm.Text =$"Term:{ selectedPM.Term}";
+                lblParty.Text =$"Party:{ selectedPM.Party}";
+               
+                string imagePath = $"{selectedPM.LastName}.jpg";
+                picPhoto.ImageLocation = imagePath;
+            }
+        }
+
+        private void frmPrimeMinisters_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
